2825043d3f65c196df5451da7cbc9b0d14fc21c5
jacob-csci <jacob.rich06@gmail.com>
2024-09-12 21:53:26 -0500

    need to fix abilities

--- src/PokedexAppGui.java
+++ src/PokedexAppGui.java
@@ -22,12 +22,20 @@ public class PokedexAppGui extends JFrame {
     private JLabel secondType;
     private JLabel height;
     private JLabel weight;
+    private JLabel hp;
+    private JLabel attack;
+    private JLabel defense;
+    private JLabel specAtt;
+    private JLabel specDef;
+    private JLabel speed;
+    private JLabel ability;
+    private JLabel hiddenAbility;
 
     public PokedexAppGui() {
         super("Pokedex");
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(350, 550);
+        setSize(350, 800);
         setLocationRelativeTo(null);
         setLayout(null);
         setResizable(false);
@@ -93,6 +101,47 @@ public class PokedexAppGui extends JFrame {
         pokemonType2.setBounds(200, 400, 450, 100);
         pokemonType2.setFont(new Font("Dialog", Font.BOLD, 40));
         add(pokemonType2);
+
+        hp = new JLabel("HP: 60");
+        hp.setBounds(50, 500, 100, 54);
+        hp.setFont(new Font("Dialog", Font.BOLD, 15));
+        add(hp);
+
+        attack = new JLabel("Attack: 60");
+        attack.setBounds(50, 550, 100, 54);
+        attack.setFont(new Font("Dialog", Font.BOLD, 15));
+        add(attack);
+
+        defense = new JLabel("Defense: 60");
+        defense.setBounds(50, 600, 100, 54);
+        defense.setFont(new Font("Dialog", Font.BOLD, 15));
+        add(defense);
+
+        specAtt = new JLabel("SpecAtt: 60");
+        specAtt.setBounds(200, 500, 100, 54);
+        specAtt.setFont(new Font("Dialog", Font.BOLD, 15));
+        add(specAtt);
+
+        specDef = new JLabel("SpecDef: 60");
+        specDef.setBounds(200, 550, 100, 54);
+        specDef.setFont(new Font("Dialog", Font.BOLD, 15));
+        add(specDef);
+
+        speed = new JLabel("Speed: 60");
+        speed.setBounds(200, 600, 100, 54);
+        speed.setFont(new Font("Dialog", Font.BOLD, 15));
+        add(speed);
+
+        ability = new JLabel("<html> Abilities: <br> Static </html>");
+        ability.setBounds(125, 675, 100, 54);
+        ability.setFont(new Font("Dialog", Font.BOLD, 15));
+        add(ability);
+
+        hiddenAbility = new JLabel("Lightnrod");
+        hiddenAbility.setBounds(125, 700, 100, 54);
+        hiddenAbility.setFont(new Font("Dialog", Font.BOLD, 15));
+        add(hiddenAbility);
+
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -148,8 +197,31 @@ public class PokedexAppGui extends JFrame {
         JSONObject type = (JSONObject) ((JSONObject) types.get(0)).get("type");
         String typeName = (String) type.get("name");
 
+        JSONArray stats = (JSONArray) pokemon.get("stats");
+        long baseHp = (long) ((JSONObject) stats.get(0)).get("base_stat");
+        long baseAtt = (long) ((JSONObject) stats.get(1)).get("base_stat");
+        long baseDef = (long) ((JSONObject) stats.get(2)).get("base_stat");
+        long baseSpecAtt = (long) ((JSONObject) stats.get(3)).get("base_stat");
+        long baseSpecDef = (long) ((JSONObject) stats.get(4)).get("base_stat");
+        long baseSpd = (long) ((JSONObject) stats.get(5)).get("base_stat");
+
+        hp.setText("HP: " + String.valueOf(baseHp));
+        attack.setText("Attack: " + String.valueOf(baseAtt));
+        defense.setText("Defense: " + String.valueOf(baseDef));
+        specAtt.setText("SpecAtt: " + String.valueOf(baseSpecAtt));
+        specDef.setText("SpecDef: " + String.valueOf(baseSpecDef));
+        speed.setText("Speed: " + String.valueOf(baseSpd));
+
+        JSONArray abilities = (JSONArray) pokemon.get("abilities");
+        JSONObject ability1 = (JSONObject) ((JSONObject) abilities.get(0)).get("ability");
+        JSONObject ability2 = (JSONObject) ((JSONObject) abilities.get(1)).get("ability");
+
+        String abil = (String) ability1.get("name");
+        String abil2 = (String) ability2.get("name");
+
+        ability.setText("<html> Abilities: <br>" + abil + "</html>");
+        hiddenAbility.setText(abil2);
         
-       
         pokemonType.setText(typeName.substring(0, 1).toUpperCase() + typeName.substring(1));
         if(types.size() > 1){
             JSONObject type2 = (JSONObject) ((JSONObject) types.get(1)).get("type");
@@ -171,6 +243,10 @@ public class PokedexAppGui extends JFrame {
                 String shinySpriteUrl = (String) sprites.get("front_shiny");
                 pokemonImage.setIcon(loadImageFromUrl(shinySpriteUrl));
             }
+
+    }
+
+    private void pokemonStats() {
 
     }
 
